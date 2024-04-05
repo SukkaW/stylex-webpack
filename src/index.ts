c5b8d7d53ffcf83072c3e602b70f120bae6f027e
SukkaW <isukkaw@gmail.com>
2024-04-05 20:56:20 +0800

    fix: source inject to asset

--- src/index.ts
+++ src/index.ts
@@ -213,7 +213,7 @@ export class StyleXPlugin {
           }
 
           // Let's find the css file that belongs to the stylex chunk
-          const cssAssetDetails = Object.entries(assets).find(([assetName]) => stylexChunk.files.has(assetName) && assetName.includes(STYLEX_CHUNK_NAME));
+          const cssAssetDetails = Object.entries(assets).find(([assetName]) => stylexChunk.files.has(assetName) && assetName.endsWith('.css'));
 
           if (!cssAssetDetails) {
             return;
