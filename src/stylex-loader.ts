218f1c572ba2b267c110bd7e59a09e385aab5db5
SukkaW <isukkaw@gmail.com>
2025-08-31 00:47:56 +0800

    refactor(loader): remove postfix import from loader

--- src/stylex-loader.ts
+++ src/stylex-loader.ts
@@ -2,9 +2,7 @@ import type webpack from 'webpack';
 import { transformAsync as babelTransformAsync } from '@babel/core';
 import stylexBabelPlugin from '@stylexjs/babel-plugin';
 import type { Options as StyleXOptions } from '@stylexjs/babel-plugin';
-import { stringifyRequest } from './lib/stringify-request';
-import { VIRTUAL_CSS_PATH, isSupplementedLoaderContext } from './constants';
-import loaderUtils from 'loader-utils';
+import { isSupplementedLoaderContext } from './constants';
 
 const PLUGIN_NAME = 'stylex';
 
@@ -18,8 +16,7 @@ export default async function stylexLoader(this: webpack.LoaderContext<StyleXLoa
   const callback = this.async();
   const {
     stylexImports,
-    stylexOption,
-    nextjsMode
+    stylexOption
   } = this.getOptions();
 
   // bail out early if the input doesn't contain stylex imports
@@ -74,42 +71,7 @@ export default async function stylexLoader(this: webpack.LoaderContext<StyleXLoa
       metadata.stylex as any
     );
 
-    // color: #fff is not url safe
-    const serializedStyleXRules = JSON.stringify(metadata.stylex);
-
-    const urlParams = new URLSearchParams({
-      from: this.resourcePath,
-      stylex: serializedStyleXRules
-    });
-
-    if (!nextjsMode) {
-      // Normal webpack mode
-
-      // We generate a virtual css file that looks like it is relative to the source
-      const virtualFileName = loaderUtils.interpolateName(
-        this as any,
-        '[path][name].[hash:base64:8].stylex.virtual.css',
-        { content: serializedStyleXRules }
-      );
-
-      const virtualCssRequest = stringifyRequest(
-        this,
-        `${virtualFileName}!=!${VIRTUAL_CSS_PATH}?${urlParams.toString()}`
-      );
-      const postfix = `\nimport ${virtualCssRequest};`;
-
-      return callback(null, code + postfix, map ?? undefined);
-    }
-
-    // Next.js App Router doesn't support inline matchResource and inline loaders
-    // So we adapt Next.js' "external" css import approach instead
-    const virtualCssRequest = stringifyRequest(
-      this,
-      `${VIRTUAL_CSS_PATH}?${urlParams.toString()}`
-    );
-    const postfix = `\nimport ${virtualCssRequest};`;
-
-    return callback(null, code + postfix, map ?? undefined);
+    return callback(null, code ?? undefined, map ?? undefined);
   } catch (error) {
     return callback(error as Error);
   }
